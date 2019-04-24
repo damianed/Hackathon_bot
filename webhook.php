c1690ddc1ce228b11821f39d2ea0e6d2ef57182a
Cosme Escobedo <comeoes@gmail.com>
2019-04-24 12:23:30 -0500

    added a some php to test

--- webhook.php
+++ webhook.php
@@ -1,14 +1,19 @@
 <?php 
+
 	$request = file_get_contents("php://input");
 	$requestJson = json_decode($request, true);
 
 	$intentDisplayName = $requestJson['queryResult']['intent']['displayName'];
 	switch ($intentDisplayName) {
 		case 'partBrand':
-			# code...
+			
+
 			break;
 		case 'search_part_number':
-			# code ...
+			$fulfillment = array(
+   			    "fulfillmentText" => "si funciono";
+   			);
+   			echo(json_encode($fulfillment));
 			break;
 		case 'engine':
 			# code ...

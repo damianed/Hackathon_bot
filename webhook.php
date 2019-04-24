b23496a09a2eecfe1fa57a83f124e553a4beb441
Cosme Escobedo <comeoes@gmail.com>
2019-04-24 17:44:56 -0500

    Added the search_part_number for testing

--- webhook.php
+++ webhook.php
@@ -1,21 +1,45 @@
-<?php 
-
+<?php
+	require('partsTech.php');
+	require('translate.php');
 	$request = file_get_contents("php://input");
 	$requestJson = json_decode($request, true);
 
 	$intentDisplayName = $requestJson['queryResult']['intent']['displayName'];
 	$params = $requestJson['queryResult']['parameters'];
+	$partsTech = new PartsTech();
 	switch ($intentDisplayName) {
 		case 'partBrand':
-			
+
 
 			break;
 		case 'search_part_number':
-			if(isset($params['part_number'])) {
-				$response = "Estas buscando el parte ".$params['part_number']."?";
-			} else {
-				$response = "No entendi tu pregunta";
+			$stores = $partsTech->getStore();
+
+			$partsByStore = [];
+
+			$searchParams = [	"partNumber" => [$params['part_number']]];
+			foreach ($stores as $store) {
+				$storeId = $store['id'];
+				$parts = $partsTech->requestQuote($searchParams, $storeId)['parts'];
+				$storeData = [];
+				$storeData['name'] = $store['name'];
+				$storeData['supplierName'] = $store['supplier']['name'];
+				$storeData['parts'] = [];
+				foreach ($parts as $part) {
+					$partName = translate($part['partName'], 'en-es');
+					$storeData['parts'][] = ['partName' => $partName, 'price' => $part['price']['list']], 'quantity' => $part['availability']['quantity']];
+				}
+				$partsByStore[] = $storeData;
 			}
+
+			$response = "Ahorita tenemos disponibles siguientes piezas disponibles en estas tiendas: ";
+			foreach ($partsByStore as $storeData) {
+				$response .= "En la tienda de " . $storeData['supplierName'] ." que esta en ". $storeData['name'].":";
+				foreach ($storeData['parts'] as $part) {
+					$response .= 'Hay '.$part['quantity'].' '.$part['partName']. ' con precio de '. $part['price'];
+				}
+			}
+
 
 			$fulfillment = array(
    			    "fulfillmentText" => $response
@@ -31,10 +55,9 @@
 		case 'SearchPartName':
 			# code ...
 			break;
-			
+
 		default:
 			# code...
 			break;
 	}
 ?>
-

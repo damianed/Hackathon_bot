0fa02f78cec3111bb32a750684ef124d1ccb6372
Paul Estala <pestala495@gmail.com>
2019-04-24 18:15:46 -0600

    submodel

--- webhook.php
+++ webhook.php
@@ -5,6 +5,7 @@
 
 	$intentDisplayName = $requestJson['queryResult']['intent']['displayName'];
 	$params = $requestJson['queryResult']['parameters'];
+	$partsTech = new PartsTech();
 	switch ($intentDisplayName) {
 		case 'partBrand':
 			
@@ -26,7 +27,36 @@
 			# code ...
 			break;
 		case 'submodel':
-			# code ...
+			if(empty($params['submodel'])) {
+				$response = "No me mandaste ningun modelo, ¿Cual es el model de tu carro?";
+			} 
+			else {
+				$solicitedYear = $params['outputContexts'][1]['parameters']['year'];
+				$solicitedMake  = $params['outputContexts'][0]['parameters']['submodel'];
+				$availableMakes = $partsTech.getMakes($year, "", "");
+				foreach ($availableMakes as $key => $make) {
+					$makeName = $make['makeName'];
+					if ($makeName == $solicitedMake) {
+						$id = $make[$key]['makeId'];
+					}
+				}		
+				if (empty($id)) {
+					$solicitedModel = $params['outputContexts'][1]['parameters']['model'];
+					$submodels = $partsTech.getSubModels($year, $solicitedMake, $solicitedModel, "");
+					$response = 'No encontre la version de tu carro con ese nombre, ¿Seguro que lo escribiste bien? Las versiones de tu carro son: ';
+					foreach ($submodels as $key => $submodel) {
+							$response += $submodel['submodelName'].', '
+					}
+				}
+				else {
+					$response = 'Cual es el Motor de tu carro?';
+				}
+			}
+			
+			$fulfillment = array(
+				"fulfillmentText" => $response
+			);
+			echo(json_encode($fulfillment));
 			break;
 		case 'SearchPartName':
 			# code ...

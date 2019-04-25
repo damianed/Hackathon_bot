3909a0753e32914e5890e44c0d096f6a5282c1bc
Damian Escobedo <damian050697@gmail.com>
2019-04-24 18:08:32 -0600

    SearchPartName

--- webhook.php
+++ webhook.php
@@ -5,6 +5,7 @@
 
 	$intentDisplayName = $requestJson['queryResult']['intent']['displayName'];
 	$params = $requestJson['queryResult']['parameters'];
+	$partsTech = new PartsTech();
 	switch ($intentDisplayName) {
 		case 'partBrand':
 			
@@ -29,7 +30,56 @@
 			# code ...
 			break;
 		case 'SearchPartName':
-			# code ...
+			$year = $params['year'];
+			$makeName = $params['make'];
+			$modelName = $params['model'];
+			$makeId = 0;
+			$allMakes = $partsTech->getMakers($year);
+			foreach($allMakes as $make){
+				if($make["makeName"] == $makeName){
+					$makeId = $make["makeId"];
+					break;
+				}
+			}
+			if($makeId == 0){
+				$fulfillment = array(
+					"fulfillmentText" => "No encontre la marca ".$makeName.", ¿Estas seguro que lo escribiste bien?"
+				);
+				echo(json_encode($fulfillment));
+				break;
+			}
+
+			$models = $partsTech->getModels($year, $makeId);
+			$modelId = 0;
+			foreach($models as $model){
+				if($model["modelName"] == $modelName){
+					$modelId = $model["modelId"];
+					break;
+				}
+			}
+			if($modelId == 0){
+				$fulfillment = array(
+					"fulfillmentText" => "No encontre la marca ".$modelName.", ¿Estas seguro que lo escribiste bien?"
+				);
+				echo(json_encode($fulfillment));
+				break;
+			}
+			$subModels = $partsTech->getSubModels($year, $make, $modelId);
+			$response = "¿De cual versión es: ";
+			foreach($subModels as $index=>$submodel){
+				if($index < sizeOf())
+				$response .= "$subModel";
+				if($index < sizeof($subModels) - 2){
+					$response .= ", ";
+				}else if($index < sizeof($subModels)){
+					$respose .= "o ";
+				}
+			}
+			$response .= "?";
+			$fulfillment = array(
+				"fulfillmentText" => $response,
+			);
+			echo(json_encode($fulfillment));
 			break;
 			
 		default:
